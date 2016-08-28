ac8440c374eee75f46a263dafe7f1b779eb45832
Florian <florian.schwanz@interoberlin.de>
2016-08-28 13:07:25 +0200

    Fixed characteristics dialog

--- app/src/main/java/de/interoberlin/poisondartfrog/view/adapters/CharacteristicsAdapter.java
+++ app/src/main/java/de/interoberlin/poisondartfrog/view/adapters/CharacteristicsAdapter.java
@@ -14,8 +14,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.interoberlin.merlot_android.controller.DevicesController;
-import de.interoberlin.merlot_android.model.config.repository.Characteristic;
-import de.interoberlin.merlot_android.model.config.repository.RepositoryMapper;
+import de.interoberlin.merlot_android.model.config.ECharacteristic;
 import de.interoberlin.poisondartfrog.R;
 
 public class CharacteristicsAdapter extends ArrayAdapter<BluetoothGattCharacteristic> {
@@ -85,11 +84,12 @@ public class CharacteristicsAdapter extends ArrayAdapter<BluetoothGattCharacteri
             viewHolder = (ViewHolder) v.getTag();
         }
 
-        Characteristic c = RepositoryMapper.getInstance(getContext()).getCharacteristicById(characteristic.getUuid().toString());
+        //Characteristic c = RepositoryMapper.getInstance(getContext()).getCharacteristicById(characteristic.getUuid().toString());
+        ECharacteristic c = ECharacteristic.fromId(characteristic.getUuid().toString());
 
         // Set values
         viewHolder.tvId.setText(characteristic.getUuid().toString().substring(4, 8));
-        viewHolder.tvName.setText(c != null ? c.getName() : getContext().getString(R.string.unknown_characteristic));
+        viewHolder.tvName.setText(c != null ? c.toString() : getContext().getString(R.string.unknown_characteristic));
 
         return v;
     }
